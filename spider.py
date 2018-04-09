87bdd3b0c931dad9df1d80621948f7c64aeb754e
Artur Niyazov <artur@niyazoff.ru>
2018-04-09 14:32:05 +0300

    it seems to be working

--- spider.py
+++ spider.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3.6
 
-import lxml
-import sys
 import multiprocessing.dummy as dmp
 
 from datetime import datetime
-
 from functions import get_entries_links, get_tree, get_link_details, put_in_db
+from update_coords import update_gps_routelen
 
 url = "https://www.domofond.ru/arenda-odnokomnatnyh-kvartir-sankt_peterburg-c3414" \
       "?PriceTo=20000&RentalRate=Month&Page={}"
@@ -18,53 +16,6 @@ def get_pages_count(url):
     # get total pages count
     pages_container = tree.xpath('//div[@class = "b-pager"]/ul/li[last()]')[0]
     return int(pages_container.text_content())
-
-#
-# links = {}
-#
-# print('Total pages:', pages_count)
-#
-# # get links from all other pages
-# for pc in range(1, pages_count + 1):
-#     print('Gathering links', pc, '\r')
-#     current_url = url.format(pc)
-#     current_tree = get_tree(current_url, url.format(1))
-#     current_links = proceed_page(current_tree)
-#     current_links = list(set(current_links))
-#     links.update({
-#         current_url: current_links
-#     })
-#
-#     # sleep(1)
-#     # break
-#
-# gathered_info = []
-#
-# c = 1
-# cw = 0
-# for l_url, l_links in links.items():
-#     print("{}/{}, {}".format(c, len(links), len(l_links)))
-#     # print(l_url, l_links)
-#     for l in l_links:
-#         try:
-#             # print(l, l_url)
-#             details = get_link_details("https://www.domofond.ru" + l, l_url)
-#             # print(details)
-#
-#             gathered_info.append(details)
-#         except Exception as e:
-#             print(e)
-#
-#         break
-#
-#     c += 1
-#
-#     # sleep(0.5)
-#     # break
-#
-# print(gathered_info)
-# # put_in_db(gathered_info)
-# # update_gps_routelen()
 
 
 def _gld_wrapper(params):
@@ -115,4 +66,8 @@ if __name__ == '__main__':
             counter += 1
         pages_counter += 1
 
-    print(gathered_info)
+    # print(gathered_info)
+    put_in_db(gathered_info)
+    update_gps_routelen()
+
+    print(f'Total elapsed time: {datetime.now() - _now}')
